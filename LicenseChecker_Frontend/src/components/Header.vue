b102c4ccf98fa14cd59a76858800396473000a94
ac142106 <iqra.imran@ub.uni-stuttgart.de>
2024-02-22 16:14:55 +0100

    Home Icon added and UI update of LR

--- LicenseChecker_Frontend/src/components/Header.vue
+++ LicenseChecker_Frontend/src/components/Header.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="q-mx-xl q-mt-md">
     <q-toolbar class="q-no-bg no-border">
-      <router-link
-        to="/"
-        @click="$emit('changeHeaderLabel', 'License Checker')"
-        v-if="header_label != 'License Checker'"
-      >
-        <button variant="outline-primary">
+      <router-link to="/" @click="$emit('changeHeaderLabel', 'License Checker')" v-if="header_label != 'License Checker'">
+        <q-btn size="md" flat color="primary" icon="img:/home.svg"></q-btn>
+        <!-- <button variant="outline-primary">
           <i class="fa fa-home"></i>
-        </button>
+        </button> -->
       </router-link>
-      <q-toolbar-title class="text-center text-primary text-h3">{{
+      <q-toolbar-title class="text-center text-primary text-h4">{{
         header_label
       }}</q-toolbar-title>
       <q-btn size="md" flat color="primary" icon="menu"></q-btn>
@@ -27,7 +24,8 @@ export default {
 };
 </script>
 <style scoped>
-h3 {
+h4 {
   color: #2f60ac;
+
 }
 </style>
