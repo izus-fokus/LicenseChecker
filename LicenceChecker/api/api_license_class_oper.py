ce94cf7376f247ce5630549a6f9cfd9d4a27afce
Usman Ghani Amin <usman-ghani.amin@ub.uni-stuttgart.de>
2024-04-03 14:11:32 +0200

    Updated License ID

--- LicenceChecker/api/api_license_class_oper.py
+++ LicenceChecker/api/api_license_class_oper.py
@@ -266,13 +266,14 @@ def check_python_dependency(my_file):
                     res = (list(filter(lambda x: 'License' in x, json['info']['classifiers'])))
                     #print(type(res))
                     lic_name=re.match('([\w\s]*)(::)([\w\s]*)(:: )([\w\s]*)',res[0])
+                   
                     data[lic]= {"license_name":lic_name.group(5),
-                                "license_id":["MIT"]}
+                                 "license_id":["MIT"]}
                     #t.add_row((lic, lic_name.group(5)))
                 elif(len(json['info']['license'])!=0):
                     lic_name=json['info']['license']
                     data[lic]={"license_name":lic_name,
-                               "license_id":["MIT"]}
+                               "license_id":["BSD"]}
                     #t.add_row((lic, lic_name))
                 else:
                      lic_name= "No record found"
